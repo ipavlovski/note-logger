e67cf27e064ffd2e98094097f90c80ceb6429f5f
ipavlovski <ilya.pavlovski@gmail.com>
2021-07-06 09:26:51 -0700

    Refactor markdown logic from router

--- src/router.ts
+++ src/router.ts
@@ -1,36 +1,8 @@
 import express from "express"
-import MarkdownIt from 'markdown-it'
 import fs from 'fs/promises'
-import sh from 'shelljs'
-import hljs from 'highlight.js'
+import { files, md } from 'src/markdown'
 
 const router = express.Router()
-
-// list all files in the dir
-function getMarkdownFiles(dirs: string, regex: RegExp): string[] {
-    return sh.ls(dirs).filter(v => regex.test(v.toString()))
-}
-
-const files = getMarkdownFiles('~/.notes/**/*.md', /hardware.*code/)
-console.log("NUMBER OF FILES: %s", files.length)
-
-var options = {
-    highlight: function (str: string, lang: string, attrs: string) {
-        if (lang && hljs.getLanguage(lang)) {
-            try { 
-                return hljs.highlight(lang, str).value 
-            } catch (error) { 
-                console.error(error) 
-            }
-        }
-
-        return ''
-    }
-}
-var md = MarkdownIt(options)
-
-
-
 
 
 router.get('/', async (req, res) => {
