f182891de4d85c554cb78f1fef828d5e95b6e93b
ipavlovski <ilya.pavlovski@gmail.com>
2022-01-27 15:32:39 -0400

    Add children-based recursive cat logic to View file

--- frontend/code/state/view.ts
+++ frontend/code/state/view.ts
@@ -138,3 +138,38 @@ export class View {
     }
 
 }
+
+
+
+// RECURSIVE CATEGORY TREE
+class CatTree {
+    tree: CategoryLeaf[]
+    constructor(input: CategoryLeaf[]) {
+        this.tree = this.buildCatTree(input)
+    }
+
+    private buildCatTree(input: CategoryLeaf[]) {
+        const recurse = ((root: CategoryLeaf) => {
+            root.children = input.filter(v => v.pid == root.id)
+            if (root.children.length > 0) root.children.map(recurse)
+            return root
+        })
+        return input.filter(v => v.pid == null).map(recurse)
+    }
+
+    get(id: number) {
+        let result: CategoryLeaf[]
+        const recurse = (id: number, tree: CategoryLeaf[], acc: CategoryLeaf[]) => {
+            for (const leaf of tree) {
+                if (leaf.id == id) result = acc.concat(leaf)
+                if (leaf.children.length > 0) recurse(id, leaf.children, acc.concat(leaf))
+            }
+        }
+        recurse(id, this.tree, [])
+        return result
+    }
+}
+// var allCats = await testdb.all<CategoryLeaf>(`select * from category`, [])
+// var catTree = new CatTree(allCats)
+// catTree.get(275)
+// catTree.tree[3].children[2].children[1].children[3].id;
