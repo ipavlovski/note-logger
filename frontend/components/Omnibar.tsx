438e5b428c44b58eb6d49e370aa235f35c0aac57
ipavlovski <ilya.pavlovski@gmail.com>
2022-05-21 08:28:33 -0300

    Omnibar options and styling

--- frontend/components/Omnibar.tsx
+++ frontend/components/Omnibar.tsx
@@ -1,11 +1,17 @@
-import React, { ClipboardEvent, useState } from 'react'
-import Select from 'react-select'
+import React, { ClipboardEvent, KeyboardEventHandler, useState } from 'react'
+import Select, { StylesConfig } from 'react-select'
+import CreatableSelect from 'react-select/creatable'
+import { ActionMeta, OnChangeValue } from 'react-select'
+
+import chroma from 'chroma-js'
+
+interface Option {
+  readonly label: string
+  readonly value: string
+  readonly color: string
+}
 
 export default function Omnibar() {
-  const dostuff = () => {
-    console.log('uh oh!!!')
-  }
-
   const [imgURL, setImgURL] = useState('https://picsum.photos/420/320')
   const [msg, setMsg] = useState('this is a message...')
 
@@ -65,11 +71,133 @@ export default function Omnibar() {
       }
     })
   }
+
+  const [value, setValue] = useState([{ label: 'lol', value: 'lol1', color: chroma.random().hex() }])
+  const [inputValue, setInputValue] = useState('')
+
+  // this handles different default (clear/remove) actions
+  // new 'modified' value is passed through it, but it needs to be 'handled'
+  const handleChange = (value: OnChangeValue<Option, true>, actionMeta: ActionMeta<Option>) => {
+    console.log(`action: ${actionMeta.action}`)
+    setValue([...value])
+  }
+
+  // handle changes to the 'total input' - get the full current value
+  const handleInputChange = (inputValue: string) => {
+    setInputValue(inputValue)
+  }
+
+  // handle changes of the individual keys
+  // able to detect each key and process it
+  const handleKeyDown: KeyboardEventHandler<HTMLDivElement> = event => {
+    // if no input, then prevent enter/tab from doing anything
+    if (!inputValue) return
+
+    // use Enter and Tab as 'confirm' keys
+    switch (event.key) {
+      case 'Enter':
+      case 'Tab':
+        // don't do anything if the the label already exists
+        if (value.find(v => v.label == inputValue)) return
+        // reset the current input value
+        setInputValue('')
+        // and set the new value
+        setValue([...value, { label: inputValue, value: inputValue, color: chroma.random().hex() }])
+        event.preventDefault()
+    }
+  }
+
+  // change components of the react-select
+  const components = { DropdownIndicator: null }
+
+  const colourStyles: StylesConfig<Option, true> = {
+    control: styles => ({ 
+      ...styles, 
+      borderRadius: 50,
+      height: '1rem',
+      backgroundColor: 'white' }),
+
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      const color = chroma(data.color)
+      // const color = chroma.random()
+      // const color = chroma('#fefefe')
+      return {
+        ...styles,
+        backgroundColor: isDisabled
+          ? undefined
+          : isSelected
+          ? data.color
+          : isFocused
+          ? color.alpha(0.1).css()
+          : undefined,
+        color: isDisabled
+          ? '#ccc'
+          : isSelected
+          ? chroma.contrast(color, 'white') > 2
+            ? 'white'
+            : 'black'
+          : data.color,
+        cursor: isDisabled ? 'not-allowed' : 'default',
+
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: !isDisabled
+            ? isSelected
+              ? data.color
+              : color.alpha(0.1).css()
+            : undefined,
+        },
+      }
+    },
+
+    multiValue: (styles, { data }) => {
+      const color = chroma(data.color)
+      return {
+        ...styles,
+        borderRadius: 50,
+        backgroundColor: color.alpha(0.4).css(),
+        
+      }
+    },
+
+    multiValueLabel: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+
+      ':hover': {
+        backgroundColor: data.color,
+        borderTopLeftRadius: 50,
+        borderBottomLeftRadius: 50,
+        color: 'white',
+      },
+    }),
+
+    multiValueRemove: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+      ':hover': {
+        backgroundColor: data.color,
+        color: 'white',
+      },
+    }),
+  }
+
   return (
     <div onPasteCapture={handleClipboardEvent}>
       <h2>Omnibar:</h2>
-      <Select isClearable menuIsOpen={false} />
-      <button onClick={dostuff}> click me</button>
+      <CreatableSelect
+        isClearable
+        isMulti
+        components={components}
+        value={value}
+        inputValue={inputValue}
+        menuIsOpen={false}
+        placeholder="Type something and press enter..."
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        styles={colourStyles}
+      />
       <p>{msg}</p>
       <img src={imgURL} />
     </div>
