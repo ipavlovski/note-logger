64a19491f496112b8acc17c8548a3e4183f55bc3
ipavlovski <ilya.pavlovski@gmail.com>
2023-01-14 17:06:37 -0400

    Add rough youtube chapter (segment) logic

--- frontend/components/youtube.tsx
+++ frontend/components/youtube.tsx
@@ -1,7 +1,9 @@
-import { AspectRatio, createStyles, Popover, TextInput } from '@mantine/core'
+import { AspectRatio, createStyles, Popover, TextInput, Tooltip } from '@mantine/core'
+import { getHotkeyHandler, useDisclosure } from '@mantine/hooks'
+import { showNotification } from '@mantine/notifications'
 // import { extractYoutubeId } from 'backend/handlers'
 import { NodeWithSiblings } from 'backend/routes'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useReducer, useRef, useState } from 'react'
 import youTubePlayer from 'youtube-player'
 import PlayerStates from 'youtube-player/dist/constants/PlayerStates'
 import type { YouTubePlayer } from 'youtube-player/dist/types'
@@ -28,6 +30,9 @@ const useStyles = createStyles(theme => ({
   mainArea: {
     paddingBottom: 16,
   },
+  savedPoint: {
+    backgroundColor: '#779ad1',
+  },
 }))
 
 function extractYoutubeId(url: string) {
@@ -41,7 +46,7 @@ function extractYoutubeId(url: string) {
 export function YouTube({ node }: { node: NodeWithSiblings }) {
   const [player, setPlayer] = useState<YouTubePlayer>()
   const [duration, setDuration] = useState<number>()
-  const [timestamps, append] = useState<number[]>([])
+  // const [timestamps, append] = useState<number[]>([])
 
   const youtubeRef = useRef<HTMLDivElement>(null)
   const { classes, cx } = useStyles()
@@ -78,7 +83,6 @@ export function YouTube({ node }: { node: NodeWithSiblings }) {
       console.log('Destroying player...')
       player.destroy()
     }
-    
   }, [videoId])
 
   return (
@@ -86,63 +90,129 @@ export function YouTube({ node }: { node: NodeWithSiblings }) {
       <AspectRatio ratio={16 / 9} mx="auto">
         <div ref={youtubeRef} className={classes.player} />
       </AspectRatio>
-      <ProgressBar player={player} duration={duration} timestamps={timestamps} append={append} />
+      <ProgressBar player={player} duration={duration} chapterNodes={node.siblings.children} />
     </>
   )
 }
 
-// https://youtu.be/AHYNxpqKqwo?t=288
-// https://youtu.be/AHYNxpqKqwo?t=809
-// https://youtu.be/AHYNxpqKqwo?t=1330
+type Chapter = { id?: number; uri?: string; title?: string; millisec: number }
 type ProgressBarArgs = {
   player: YouTubePlayer | undefined
   duration: number | undefined
-  timestamps: number[]
-  append: React.Dispatch<React.SetStateAction<number[]>>
+  chapterNodes: NodeWithSiblings['siblings']['children']
 }
-function ProgressBar({ player, duration, timestamps, append }: ProgressBarArgs) {
+function ProgressBar({ player, duration, chapterNodes }: ProgressBarArgs) {
   const { classes, cx } = useStyles()
 
-  const segments = timestamps.map(v => {
-    const percent = Math.floor((v / duration!) * 100)
-    return (
-      <Popover width={300} trapFocus position="bottom" withArrow shadow="md">
-        <Popover.Target>
-          <div
-            onClick={() => {
-              console.log(player)
-              player!.seekTo(v, true)
-              console.log(`clicked on ${v}`)
-            }}
-            className={classes.point}
-            key={v}
-            style={{ left: `${percent}%` }}></div>
-        </Popover.Target>
-        <Popover.Dropdown
-          sx={theme => ({
-            background: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-          })}>
-          <TextInput label="Name" placeholder="Name" size="xs" />
-        </Popover.Dropdown>
-      </Popover>
-    )
+  // str1.match(/\[.*\]$/g)
+  // str1.replace(/\[.*\]$/, '')
+  // url1.match(/(\[)(.*)(\])$/)
+
+  const originalChapters = chapterNodes.map(node => {
+    const millisec = parseFloat(node.uri.match(/(\[)(.*)(\])$/)![2])
+    return { id: node.id, uri: node.uri, title: node.title, millisec: millisec }
   })
+  // const [chapters, setChapters] = useState<Chapter[]>(originalChapters)
+
+  const reducer = (chapters: Chapter[], incomingChapter: Chapter) => {
+    const match = chapters.find(chapter => chapter.millisec == incomingChapter.millisec)
+    if (match) {
+      match.title = incomingChapter.title
+      return chapters
+    }
+    return chapters.concat(incomingChapter)
+  }
+  const [chapters, chapterDispatch] = useReducer(reducer, originalChapters)
 
   return (
     <div
       onDoubleClick={() => {
         if (player) {
-          player.getCurrentTime().then(v => {
-            append(timestamps.concat(Math.floor(v)))
-            console.log(`time: ${v}`)
+          player.getCurrentTime().then(currentTime => {
+            chapterDispatch({
+              millisec: Math.round(currentTime * 1000) / 1000,
+            })
+            console.log(`time: ${currentTime}`)
           })
         }
       }}
       className={classes.bar}>
-      {player && duration && segments}
+      {player &&
+        duration &&
+        chapters.map(chapter => (
+          <YoutubeChapter
+            player={player}
+            duration={duration}
+            chapter={chapter}
+            key={chapter.millisec}
+            chapterDispatch={chapterDispatch}
+          />
+        ))}
     </div>
   )
 }
 
-type SegmentArg = {}
-function Segment({}: SegmentArg) {}
+type YoutubeChapterArgs = {
+  player: YouTubePlayer
+  duration: number
+  chapter: Chapter
+  chapterDispatch: React.Dispatch<Chapter>
+  // chapterNode: NodeWithSiblings['siblings']['children'][0]
+}
+function YoutubeChapter({ player, duration, chapter, chapterDispatch }: YoutubeChapterArgs) {
+  const { classes, cx } = useStyles()
+
+  const percent = Math.floor((chapter.millisec / duration) * 100)
+
+  const [opened, setOpened] = useState(false)
+
+  const [value, setValue] = useState('')
+  const handleSave = () => {
+    showNotification({ title: 'Saved', color: 'teal', message: value })
+    setOpened(false)
+    chapterDispatch({ millisec: chapter.millisec, title: value })
+    setValue('')
+
+    // const targetChapter = chapter.find(v => v)
+    // setChapters()
+  }
+
+  const target = (
+    <div
+      // on={() => setOpened(false)}
+      onClick={() => {
+        console.log(player)
+        setOpened(o => !o)
+        player!.seekTo(chapter.millisec, true)
+        console.log(`clicked on ${chapter.millisec}`)
+      }}
+      className={cx(classes.point, chapter.title != null && classes.savedPoint)}
+      style={{ left: `${percent}%` }}></div>
+  )
+
+  return chapter.title != null ? (
+    <Tooltip label={chapter.title}>{target}</Tooltip>
+  ) : (
+    <Popover
+      opened={opened}
+      width={300}
+      position="bottom"
+      onChange={setOpened}
+      withArrow
+      shadow="md">
+      <Popover.Target>{target}</Popover.Target>
+      <Popover.Dropdown
+        sx={theme => ({
+          background: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+        })}>
+        <TextInput
+          placeholder="Title goes here."
+          label="Press enter to submit..."
+          value={value}
+          onChange={event => setValue(event.target.value)}
+          onKeyDown={getHotkeyHandler([['Enter', handleSave]])}
+        />
+      </Popover.Dropdown>
+    </Popover>
+  )
+}
