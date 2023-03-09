378b0ab6cb53a754fa0e0172decaca2bfdc4a02f
ipavlovski <ilya.pavlovski@gmail.com>
2023-03-09 13:50:15 -0400

    Add pdf handler

--- frontend/components/preview.tsx
+++
@@ -1,7 +0,0 @@
-import { Skeleton } from '@mantine/core'
-
-export default function Preview({ height }: {height: string | number}) {
-  return (
-    <Skeleton animate={false} height={height} my={14} />
-  )
-}
